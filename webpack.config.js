c056612e039198b795f981568de98cc9c983f6c7
gc-Maker <71691194+gc-Maker@users.noreply.github.com>
2025-05-30 16:11:16 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// const { options } = require('less');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname + '/src/index.js'),
+    output: {
+        path: path.join(__dirname + '/dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css|less)$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.(tsx|ts|jsx|js)$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true
+                        }
+                    }
+                ],
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname + '../public/index.html')
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+        proxy: {
+            '/api': 'http://localhost:5000',
+        },
+    },
+    mode: 'development'
+};
